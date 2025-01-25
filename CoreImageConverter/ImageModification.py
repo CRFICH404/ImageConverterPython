ca479764827b6358f2193772ca60b64c01bb095e
CRFICH404 <portniagin@seznam.cz>
2025-01-25 14:48:01 +0100

    Made image resize function

--- CoreImageConverter/ImageModification.py
+++ CoreImageConverter/ImageModification.py
@@ -2,27 +2,68 @@
 from functools import singledispatch
 from PIL import Image
 import numpy as np
+import math
 
 
+def change_image(image_name: str, command: str, change_values=None):
+    if change_values is None:
+        change_values = []
+    match command:
+        case "crop_image":
+            crop_image(open_image(image_name), change_values[0], change_values[1])
+        case "rotate_image_right_90_deg":
+            rotate_image_right_90_deg(open_image(image_name))
+        case "flip_image":
+            flip_image(open_image(image_name))
+        case "solarize_image":
+            if len(change_values) < 1:
+                solarize_image(open_image(image_name))
+            elif len(change_values) == 1:
+                solarize_image(open_image(image_name), change_values[0])
+            else:
+                solarize_image(open_image(image_name), change_values[0], change_values[1])
+        case "convert_to_negative":
+            convert_to_negative(open_image(image_name))
+        case "change_brightness":
+            if len(change_values) < 1:
+                change_brightness(open_image(image_name), 0)
+            else:
+                change_brightness(open_image(image_name), change_values[0])
+        case "change_contrast":
+            if len(change_values) < 1:
+                change_contrast(open_image(image_name), 1)
+            else:
+                change_contrast(open_image(image_name), change_values[0])
+        case _:
+            print("No such command")
+    ...
 
-def open_image (address: str) -> np.ndarray:
-    if is_grayscale(address):
-        image = np.array(Image.open(address))
+def open_image (image_name: str) -> np.ndarray:
+
+    path_base = 'C:\\Users\\zazon\\PycharmProjects\\ImageConverter\\ServerApp\\images_upload\\'
+    path_to_image = path_base + image_name
+
+    if is_grayscale(path_to_image):
+        image = np.array(Image.open(path_to_image))
         print(image.shape)
         return np.dot(image[..., :3], [0.2989, 0.5870, 0.1140])
-    return np.array(Image.open(address))
+    return np.array(Image.open(path_to_image))
 
 @singledispatch
-def is_grayscale (img):
+def is_grayscale (imag):
     raise NotImplementedError("Not implemented")
 
 @is_grayscale.register(np.ndarray)
-def _(img)->bool:
-    return True
+def _(imag)->bool:
+    if len(imag.shape) <= 2:
+        print("Grayscale image")
+        return True
+    print("Color image")
+    return False
 
 @is_grayscale.register(str)
-def _(img)->bool:
-    image = np.array(Image.open(img))
+def _(imag)->bool:
+    image = np.array(Image.open(imag))
     image_total_pixel_diff = 0
     sample_crop_from = [int(image.shape[0] / 2 - image.shape[0] / 20), int(image.shape[1] / 2 - image.shape[1] / 20)]
     sample_crop_to = [int(image.shape[0] / 2 + image.shape[0] / 20), int(image.shape[1] / 2 + image.shape[1] / 20)]
@@ -72,13 +113,10 @@ def convert_to_negative (image: np.ndarray) -> np.ndarray:
 def solarize_image (image: np.ndarray, thresh_val: int = 130, effect : int = 255) -> np.ndarray:
     if not is_grayscale(image):
         red_image, green_image, blue_image = image[:, :, 0], image[:, :, 1], image[:, :, 2]
-
         red_solarization = np.where(red_image < thresh_val, red_image, effect - red_image)
         green_solarization = np.where(green_image < thresh_val, green_image, effect - green_image)
         blue_solarization = np.where(blue_image < thresh_val, blue_image, effect - blue_image)
-
         image_solarization = np.dstack((red_solarization, green_solarization, blue_solarization))
-
     else:
         image_solarization = np.where(image < thresh_val, image, effect - image)
 
@@ -91,6 +129,7 @@ def change_brightness (image: np.ndarray, value: int) -> np.ndarray:
     return image_br.astype(np.uint8)
 
 def change_contrast (image: np.ndarray, value: float = 0) -> np.ndarray:
+    #need to lookup better ways to change contrast
     if value > 49.9:
         print("Value cannot be bigger than 49.9")
         return image
@@ -100,14 +139,62 @@ def change_contrast (image: np.ndarray, value: float = 0) -> np.ndarray:
 
     return image_changed.astype(np.uint8)
 
+def resize_image (image: np.ndarray, new_height: int | float, new_width: int | float) -> np.ndarray:
+    grayscale = is_grayscale(image)
+    new_width = int(new_width)
+    new_height = int(new_height)
+    if not grayscale:
+        new_image = np.zeros((math.floor(new_height), math.floor(new_width), image.shape[2]), dtype=np.uint8)
+        print(image.shape)
+    else:
+        new_image = np.zeros((math.floor(new_height), math.floor(new_width)), dtype=np.uint8)
+
+    image_scale_factor_y = image.shape[0] / new_height
+    image_scale_factor_x = image.shape[1] / new_width
+
+    for new_y in range(new_height):
+        for new_x in range(new_width):
+            if grayscale:
+                old_pixel = [new_y * image_scale_factor_y, new_x * image_scale_factor_x]
+                pixel_fraction = [old_pixel[0] - math.floor(old_pixel[0]), old_pixel[1] - math.floor(old_pixel[1])]
+
+                left_upper_pixel = image[math.floor(old_pixel[0]), math.floor(old_pixel[1])]
+                right_upper_pixel = image[math.floor(old_pixel[0]), min(image.shape[1] - 1 ,math.ceil(old_pixel[1]))]
+                left_lower_pixel = image[min(image.shape[0] - 1 ,math.ceil(old_pixel[0])), math.floor(old_pixel[1])]
+                right_lower_pixel = image[min(image.shape[0] - 1 ,math.ceil(old_pixel[0])), min(image.shape[1] - 1 ,math.ceil(old_pixel[1]))]
+
+                mix_top_pixels = left_upper_pixel * pixel_fraction[1] + right_upper_pixel * (1 - pixel_fraction[1])
+                mix_bot_pixels = left_lower_pixel * pixel_fraction[1] + right_lower_pixel * (1 - pixel_fraction[1])
+                mix_top_and_bot = mix_top_pixels * pixel_fraction[0] + mix_bot_pixels * (1 - pixel_fraction[0])
+
+                new_image[new_y, new_x] = mix_top_and_bot
+
+            else:
+                for channel in range(image.shape[2]):
+                    old_pixel = [new_y * image_scale_factor_y, new_x * image_scale_factor_x]
+                    pixel_fraction = [old_pixel[0] - math.floor(old_pixel[0]), old_pixel[1] - math.floor(old_pixel[1])]
+
+                    left_upper_pixel = image[math.floor(old_pixel[0]), math.floor(old_pixel[1]), channel]
+                    right_upper_pixel = image[math.floor(old_pixel[0]), min(image.shape[1] - 1, math.ceil(old_pixel[1])), channel]
+                    left_lower_pixel = image[min(image.shape[0] - 1, math.ceil(old_pixel[0])), math.floor(old_pixel[1]), channel]
+                    right_lower_pixel = image[min(image.shape[0] - 1, math.ceil(old_pixel[0])), min(image.shape[1] - 1,math.ceil(old_pixel[1])), channel]
+
+                    mix_top_pixels = left_upper_pixel * pixel_fraction[1] + right_upper_pixel * (1 - pixel_fraction[1])
+                    mix_bot_pixels = left_lower_pixel * pixel_fraction[1] + right_lower_pixel * (1 - pixel_fraction[1])
+                    mix_top_and_bot = mix_top_pixels * pixel_fraction[0] + mix_bot_pixels * (1 - pixel_fraction[0])
+
+                    new_image[new_y, new_x, channel] = mix_top_and_bot
+
+    return new_image
+
 if __name__ == "__main__":
-    path = 'C:\\Users\\zazon\\PycharmProjects\\ImageConverter\\ServerApp\\images_upload\\Widowmaker-Overwatch-Blizzard-8731049.jpg'
-    path2 = 'C:\\Users\\zazon\\PycharmProjects\\ImageConverter\\ServerApp\\images_upload\\tiger-animals-cat-predator-preview.jpg'
-    path3 = 'C:\\Users\\zazon\\PycharmProjects\\ImageConverter\\ServerApp\\images_upload\\GO48R.png'
-    image_np = open_image(path)
-    image_np_2 = open_image(path2)
-    print(image_np.shape, image_np_2.shape)
-    Image.fromarray(change_contrast(image_np_2)).show()
+    img_name = 'honkibooty-Rebecca-(Edgerunners)-Cyberpunk-Edgerunners-8756249.jpg'
+    img_name_2 = 'tiger-animals-cat-predator-preview.jpg'
+    path3 = 'GO48R.png'
+    image_np = open_image(img_name)
+    image_np_2 = open_image(img_name_2)
+    #Image.fromarray(resize_image(image_np, 1520, 1520)).show()
+    #Image.fromarray(change_contrast(image_np_2)).show()
     #image_np_3 = open_image(path3)
     #Image.fromarray(image_np).show()
     #Image.fromarray(change_contrast(image_np)).show()
